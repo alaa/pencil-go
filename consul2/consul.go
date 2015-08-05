4845c2b491fd75c634ccecdfb8fef60a1c119212
Oleksii Fedorov <waterlink000@gmail.com>
2015-08-05 05:14:58 +0300

    [simplify] consul2 vs consul :: reach feature parity

---
+++ consul2/consul.go
@@ -0,0 +1,91 @@
+package consul2
+
+import (
+	"fmt"
+	"github.com/alaa/pencil-go/container"
+	consul "github.com/hashicorp/consul/api"
+	"os"
+)
+
+var (
+	client, clientErr = consul.NewClient(consul.DefaultConfig())
+)
+
+func Resync(containers []container.Container) error {
+	services, err := currentServiceIDs()
+	if err != nil {
+		return err
+	}
+
+	toDeregister := servicesToDeregister(services, containers)
+
+	for _, s := range toDeregister {
+		err = client.Agent().ServiceDeregister(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	toRegister := containersToRegister(services, containers)
+
+	for _, c := range toRegister {
+		id := serviceID(c)
+		err = client.Agent().ServiceRegister(&consul.AgentServiceRegistration{
+			ID:   id,
+			Name: id,
+			Tags: c.Tags,
+			Port: int(c.Port),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func currentServiceIDs() (map[string]*consul.AgentService, error) {
+	services, err := client.Agent().Services()
+	if err != nil {
+		return nil, err
+	}
+
+	delete(services, "consul")
+	return services, nil
+}
+
+func containersToRegister(running map[string]*consul.AgentService, containers []container.Container) []container.Container {
+	result := []container.Container{}
+	for _, c := range containers {
+		if _, exists := running[serviceID(c)]; !exists {
+			result = append(result, c)
+		}
+	}
+	return result
+}
+
+func servicesToDeregister(running map[string]*consul.AgentService, containers []container.Container) []string {
+	containerMap := map[string]struct{}{}
+	for _, c := range containers {
+		containerMap[serviceID(c)] = struct{}{}
+	}
+
+	result := []string{}
+	for service, _ := range running {
+		if _, exists := containerMap[service]; !exists {
+			result = append(result, service)
+		}
+	}
+	return result
+}
+
+func serviceID(c container.Container) string {
+	return fmt.Sprintf("%s-%s-%d", c.ID, c.Name, c.Port)
+}
+
+func init() {
+	if clientErr != nil {
+		fmt.Printf("Unable to connect to consul: %v\n", clientErr)
+		os.Exit(1)
+	}
+}
---
+++ consul2/consul_test.go
@@ -0,0 +1,149 @@
+package consul2
+
+import (
+	"github.com/alaa/pencil-go/container"
+	consul "github.com/hashicorp/consul/api"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	consulService = consul.AgentService{
+		ID:      "consul",
+		Service: "consul",
+		Tags:    []string{},
+		Port:    8300,
+	}
+)
+
+func TestResyncEmpty(t *testing.T) {
+	containers := []container.Container{}
+	err := Resync(containers)
+	assert.Nil(t, err)
+
+	services, err := client.Agent().Services()
+	assert.Nil(t, err)
+
+	assertSameServices(t, map[string]consul.AgentService{
+		"consul": consulService,
+	}, services)
+}
+
+func TestResyncAddContainers(t *testing.T) {
+	TestResyncEmpty(t)
+
+	containers := []container.Container{
+		container.Container{
+			ID:   "57jkhgmv67",
+			Name: "helloworld",
+			Port: 3394,
+			Tags: []string{"tag1", "tag2"},
+		},
+		container.Container{
+			ID:   "57jkhgmv67",
+			Name: "helloworld",
+			Port: 22,
+			Tags: []string{"tag1", "tag2"},
+		},
+		container.Container{
+			ID:   "99nameg643b",
+			Name: "some-test",
+			Port: 9931,
+			Tags: []string{},
+		},
+	}
+	err := Resync(containers)
+	assert.Nil(t, err)
+
+	services, err := client.Agent().Services()
+	assert.Nil(t, err)
+
+	assertSameServices(t, map[string]consul.AgentService{
+		"consul": consulService,
+		"57jkhgmv67-helloworld-3394": consul.AgentService{
+			ID:      "57jkhgmv67-helloworld-3394",
+			Service: "57jkhgmv67-helloworld-3394",
+			Port:    3394,
+			Tags:    []string{"tag1", "tag2"},
+		},
+		"57jkhgmv67-helloworld-22": consul.AgentService{
+			ID:      "57jkhgmv67-helloworld-22",
+			Service: "57jkhgmv67-helloworld-22",
+			Port:    22,
+			Tags:    []string{"tag1", "tag2"},
+		},
+		"99nameg643b-some-test-9931": consul.AgentService{
+			ID:      "99nameg643b-some-test-9931",
+			Service: "99nameg643b-some-test-9931",
+			Port:    9931,
+			Tags:    nil,
+		},
+	}, services)
+}
+
+func TestResyncRemoveAndAddContainers(t *testing.T) {
+	TestResyncAddContainers(t)
+
+	containers := []container.Container{
+		container.Container{
+			ID:   "57jkhgmv67",
+			Name: "helloworld",
+			Port: 22,
+			Tags: []string{"tag1", "tag2"},
+		},
+		container.Container{
+			ID:   "771hhjasfu34hu",
+			Name: "landing-pages",
+			Port: 9999,
+			Tags: []string{"tag3", "tag2"},
+		},
+	}
+	err := Resync(containers)
+	assert.Nil(t, err)
+
+	services, err := client.Agent().Services()
+	assert.Nil(t, err)
+
+	assertSameServices(t, map[string]consul.AgentService{
+		"consul": consulService,
+		"57jkhgmv67-helloworld-22": consul.AgentService{
+			ID:      "57jkhgmv67-helloworld-22",
+			Service: "57jkhgmv67-helloworld-22",
+			Port:    22,
+			Tags:    []string{"tag1", "tag2"},
+		},
+		"771hhjasfu34hu-landing-pages-9999": consul.AgentService{
+			ID:      "771hhjasfu34hu-landing-pages-9999",
+			Service: "771hhjasfu34hu-landing-pages-9999",
+			Port:    9999,
+			Tags:    []string{"tag3", "tag2"},
+		},
+	}, services)
+}
+
+func assertSameServices(t *testing.T, expected map[string]consul.AgentService, actual map[string]*consul.AgentService) {
+	for key, service := range expected {
+		if actual[key] == nil {
+			t.Errorf("Expected to see service %s\n", key)
+			continue
+		}
+
+		assert.Equal(t, service.ID, actual[key].ID)
+		assert.Equal(t, service.Service, actual[key].Service)
+		assert.Equal(t, service.Port, actual[key].Port)
+		assert.Equal(t, service.Tags, actual[key].Tags)
+	}
+
+	for key, _ := range actual {
+		service, found := expected[key]
+		if !found {
+			t.Errorf("Expected not to see service %s\n", key)
+			continue
+		}
+
+		assert.Equal(t, service.ID, actual[key].ID)
+		assert.Equal(t, service.Service, actual[key].Service)
+		assert.Equal(t, service.Port, actual[key].Port)
+		assert.Equal(t, service.Tags, actual[key].Tags)
+	}
+}
